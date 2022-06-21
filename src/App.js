8fe38efec03c6af79e5f4ae947a47721be068b06
Lakhwinder Sandhu <lakhwinder.singh.henceforth@gmail.com>
2022-06-21 18:11:25 +0530

    comment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Mytask from './Mytask';
+
+function App() {
+  return (
+    <div className="App">
+      <Mytask/>
+      
+    </div>
+  );
+}
+
+export default App;
